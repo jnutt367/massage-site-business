b27ada853dbef7a29861700be1e81af9932fe0af
Jason Nutt <jnutt367@gmail.com>
2020-09-09 12:28:08 -0400

    Re-Thinking from start-Navigation-first
    Scrapped most of initial styles.....learning to restart if things are getting out of control. Navigation re-structured and JavaScript functionality added to Nav Bar.

--- form.js
+++ form.js
@@ -1,5 +1,25 @@
-//   this script tag implements the form from jot.js
+var open = document.getElementById('hamburger');
+var changeIcon = true;
 
+open.addEventListener("click", function(){
 
+    var overlay = document.querySelector('.overlay');
+    var nav = document.querySelector('nav');
+    var icon = document.querySelector('.menu-toggle i');
 
-//<script src="https://form.jotform.com/jsform/202294625026148"></script>
+    overlay.classList.toggle("menu-open");
+    nav.classList.toggle("menu-open");
+
+    if (changeIcon) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+
+        changeIcon = false;
+    }
+    else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+        changeIcon = true;
+    }
+});
+
